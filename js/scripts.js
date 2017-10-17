2c3f6803e377a102c3981c5a7e0083f7464dbc22
Tomasz Andrzejewski <kasta.tomek@wp.pl>
2017-10-17 18:58:29 +0200

    po poprawkach

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,19 @@ $(function(){
 
 	var carouselList = $("#carousel ul");
 
-	$("carouselList").setInterval(changeSlide, 3000);
+	setInterval(changeSlide, 3000);
 
-	$("carouselList").animate({marginLeft: '-400'}, 500, moveFirstSlide);
+	function changeSlide() {
+		carouselList.animate({marginLeft: '-400'}, 500, moveFirstSlide);
+	}
 
-	var firstItem = $("carouselList").find("li:first");
-	var lastItem = $("carouselList").find("li:last");
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
 
-	$("lastItem").after("firstItem");
+		lastItem.after(firstItem);
 
-	$("carouselList").css({marginLeft:0});
-
-
+		carouselList.css({marginLeft:0});
+	}
 
 });
